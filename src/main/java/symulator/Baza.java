c83ebced541cc9ed7d073f5da376d2d04c99dd80
Your Name <damianstrzelczyk@o2.pl>
2020-11-01 16:24:16 +0100

    Damian Strzelczyk

--- src/main/java/symulator/Baza.java
+++ src/main/java/symulator/Baza.java
@@ -19,4 +19,21 @@ public class Baza {
         model.addAttribute("fname", fname);
         return "milosc/milosc";
     }
+
+        @GetMapping(value = "/zobaczCertyfikaty")
+        public String certyfikaty(Model model) {
+
+            return "milosc/certyfikaty";
+
+    }
+
+    @GetMapping(value = "/Hibernate")
+    public String hiberante(Model model){
+        return "milosc/Hibernate";
+    }
+    @GetMapping(value = "/C_School")
+    public String CSchool(Model model){
+        return "milosc/C_School";
+    }
+
 }
