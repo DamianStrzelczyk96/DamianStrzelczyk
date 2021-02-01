f1d6d1a16f8edc298b3db7c05478a720352b694f
Your Name <damianstrzelczyk@o2.pl>
2021-02-01 23:06:21 +0100

    Add projects and image

--- src/main/java/symulator/Baza.java
+++ src/main/java/symulator/Baza.java
@@ -8,8 +8,11 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class Baza {
 
+    int licznik = 0;
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        licznik++;
+        model.addAttribute("licznik",licznik);
         return "milosc/index";
     }
 
@@ -23,30 +26,41 @@ public class Baza {
 
         @GetMapping(value = "/zobaczCertyfikaty")
         public String certyfikaty(Model model) {
-
+        licznik++;
             return "milosc/certyfikaty";
+
+    }
+    @GetMapping(value = "/zobaczProjekty")
+    public String projekty(Model model) {
+        licznik++;
+        return "milosc/projekty";
 
     }
 
     @GetMapping(value = "/Hibernate")
     public String hiberante(Model model){
+        licznik++;
         return "milosc/Hibernate";
     }
 
     @GetMapping(value = "/C_School")
     public String CSchool(Model model){
+        licznik++;
         return "milosc/C_School";
     }
     @GetMapping(value = "/Udemy")
     public String Udemy(Model model){
+        licznik++;
         return "milosc/Udemy";
     }
     @GetMapping(value = "/JUnit")
     public String JUnit(Model model){
+        licznik++;
         return "milosc/JUnit";
     }
     @GetMapping(value = "/Wzorce")
     public String Wzorce(Model model){
+        licznik++;
         return "milosc/Wzorce";
     }
 
